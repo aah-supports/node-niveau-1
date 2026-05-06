e68ca7305c339dc8e6c554ca25153aa7dccdb133
Antoine07 <antoine.lucsko@wanadoo.fr>
2026-05-06 13:59:48 +0200

    Refactor sandbox-server: controllers/services split and Campus Quest REST correction

--- sandbox-server/src/server.js
+++ sandbox-server/src/server.js
@@ -1,37 +1,22 @@
+// point d'entrée de l'application 
+
 import express from "express";
-// import fs from "node:fs/promises";
-// const { readFile, writeFile } = fs ;
-import { readFile, writeFile } from "node:fs/promises";
+import { createPlayerController, playersController } from "./controllers/playerController.js";
+import { answerEnigmeController, enigmesController } from "./controllers/enigmeController.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
-const playersPath = new URL("../data/players.json", import.meta.url);
 
-// middleware 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.status(200).json({ hello: "WORLD" });
-});
+// endpoint connecter à son controleur
+app.get("/players", playersController);
 
-app.get("/players", async (_req, res) => {
+// POST /players
+app.post("/players", createPlayerController);
+app.get("/enigmes", enigmesController);
+app.post("/enigmes/:id/answer", answerEnigmeController);
+app.post("/enigmes/:id/answers", answerEnigmeController);
 
-  const players = await readFile(playersPath, "utf-8");
-  const playersJson = JSON.parse(players);
-
-  // "" 0 false null undefined considérer comme faux dans un test JS 
-  // ici on teste si c'est un array et pas vide 
-  if (!Array.isArray(playersJson) || playersJson.length === 0) {
-    return res.status(404).json({ response: "Aucun joueur trouvé" });
-  }
-
-  return res.status(200).json(playersJson);
-});
-
-app.post("/players", async (req, res) => {
-
-})
-
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
+app.listen(3000, () => {
+  console.log(`Server listening on 3000`);
 });
